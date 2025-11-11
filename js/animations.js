cf586464dd471e2ea06d6ba42c1a99d8112dfeb2
czuellig <cedric.zuellig@gmail.com>
2025-11-11 13:52:39 +0100

    Opener Section animation

--- js/animations.js
+++ js/animations.js
@@ -32,4 +32,58 @@ let smoother = ScrollSmoother.create({
     wrapper: ".smooth-wrapper",
     content: ".smooth-content", 
    
-})
+});
+
+let splitQuoteText = SplitText.create("#quote-text", {type:"lines"});
+
+gsap.from(splitQuoteText.lines, {
+    scrollTrigger: {
+        trigger: "#opener-quote",
+        start: "top 80%",
+    },
+    y: 50,
+    opacity: 0,
+    ease: "power2.out",
+    duration: 1,
+    stagger: 0.1,
+});
+
+let splitQuoteSource = SplitText.create("#quote-source", {type:"chars"});
+
+gsap.from(splitQuoteSource.chars, {
+    scrollTrigger: {
+        trigger: "#opener-quote",
+        start: "bottom 80%",
+        markers: true,
+    },
+    y: 50,
+    opacity: 0,
+    ease: "power2.out",
+    duration: 1,
+    stagger: 0.05,
+});
+
+// --- Szene 2: Opener – Titel bewegt sich horizontal ---
+gsap.registerPlugin(ScrollTrigger);
+
+const title = document.querySelector("#story-title");
+const titleWidth = title.scrollWidth;
+const windowWidth = window.innerWidth;
+
+// Scrolldistanz anpassen: nur so weit scrollen, bis das "E" sichtbar ist
+// → wir ziehen ca. 20% der Fensterbreite ab
+const scrollDistance = titleWidth - windowWidth * 1;
+
+gsap.to("#story-title", {
+  x: -scrollDistance,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#opener",
+    start: "top top",
+    end: `+=${scrollDistance}`, // scrollt nur bis "E" sichtbar ist
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+  },
+});
+
