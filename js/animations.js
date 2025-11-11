8cbba3c47e79ad7b2ecfc57cdac03a6cfbf0d7bf
czuellig <cedric.zuellig@gmail.com>
2025-11-11 09:50:49 +0100

    add title scrolling effect

--- js/animations.js
+++ js/animations.js
@@ -3,17 +3,30 @@ gsap.from("#title-text-container", {
     opacity: 0,
     duration: 0.8,
     ease: "power1.inOut"
-})
+});
 
-gsap.to("#title-pic-top-container", {
-    y: -500,
-    scrollTrigger: {
-        trigger: "#title",
-        start: "top top",
-        end: "bottom top",
-        scrollSpace: false,
-    }
-})
+// Animationen mit GSAP + ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+// --- Szene 1: Titelbereich mit Parallax-Effekt ---
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#title",
+    start: "top top",
+    end: "+=200%", // Länge des Scrollabschnitts
+    scrub: true,
+    pin: true, // fixiert den gesamten Bereich
+  },
+});
+
+// Text und Hintergrund bleiben stehen
+// Das obere Bild (Collage) gleitet von unten nach oben
+tl.fromTo(
+  "#title-pic-top-container",
+  { y: "100%" },   // Start: unterhalb
+  { y: "0%", ease: "none" } // Ziel: überlagert das untere Bild
+);
+
 
 let smoother = ScrollSmoother.create({
     wrapper: ".smooth-wrapper",
